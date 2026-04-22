e3a021a0c715e02ff817f0d97348b7bd0a8eff13
Getuar Jakupi <getuar.j1@gmail.com>
2026-04-22 09:46:26 +0200

    debug: log real error stack

--- jenkins-docker-run.sh
+++ jenkins-docker-run.sh
@@ -14,6 +14,15 @@
 # echo "Prit 30 sekonda dhe pastaj shiko passwordin fillestar:"
 # echo "  docker exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword"
 
+# docker run -d `
+#   --name jenkins `
+#   --restart=on-failure `
+#   -p 8080:8080 `
+#   -p 50000:50000 `
+#   -v jenkins_home:/var/jenkins_home `
+#   -v //./pipe/docker_engine://./pipe/docker_engine `
+#   jenkins/jenkins:lts-jdk17
+
 docker run -d `
   --name jenkins `
   --restart=on-failure `
@@ -21,4 +30,6 @@ docker run -d `
   -p 50000:50000 `
   -v jenkins_home:/var/jenkins_home `
   -v //./pipe/docker_engine://./pipe/docker_engine `
+  -v "${HOME}/.kube:/root/.kube:ro" `
+  -e DOCKER_HOST=npipe:////./pipe/docker_engine `
   jenkins/jenkins:lts-jdk17
